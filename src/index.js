4e062cf9dfbcc85dcb2af9a209fd833867b5db27
Paichato <tsenane14@gmail.com>
2021-08-02 16:37:26 +0200

    added pagination

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 const express=require('express');
+ const plantdata=require('./model/plantdata.json');
 
-const app=express;
+const app=express();
+
+const plantss=[{id:1,plant:"Micaia"},{id:2,plant:"Embondeiro"}]
+
+// console.log(plantdata);
+app.get("/plants",(req,res)=>{
+    const page=req.query.page;
+    const limit=req.query.limit;
+    const startIndex=(page-1)*limit;
+    const endIndex=page*limit;
+    const result=plantdata.plants.slice(startIndex,endIndex);
+
+    res.json(result);
+})
+
 
 app.listen(3000,()=>{
     console.log('server started on port 3000');
