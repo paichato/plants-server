f0cf554cd62c3e4276db138639f0724b8ae641db
Paichato <tsenane14@gmail.com>
2021-08-02 17:24:57 +0200

    added a bug fix for results

--- src/index.js
+++ src/index.js
@@ -7,12 +7,28 @@ const plantss=[{id:1,plant:"Micaia"},{id:2,plant:"Embondeiro"}]
 
 // console.log(plantdata);
 app.get("/plants",(req,res)=>{
-    const page=req.query.page;
-    const limit=req.query.limit;
-    const startIndex=(page-1)*limit;
-    const endIndex=page*limit;
-    const result=plantdata.plants.slice(startIndex,endIndex);
-
+    const _page=parseInt(req.query._page);
+    const _limit=parseInt(req.query._limit);
+    const startIndex=(_page-1)*_limit;
+    const endIndex=_page*_limit;
+    // const result=plantdata.plants.slice(startIndex,endIndex);
+console.log(_page);
+    const result={};
+   
+    if(endIndex<plantdata.plants.length){
+        result.next={
+            page:_page+1,
+            limit:_limit
+        };
+    };
+    if(startIndex>0){
+        result.previous={
+            page:_page-1,
+            limit:_limit,
+        };
+    }
+    result.results=(_page && _limit)?plantdata.plants.slice(startIndex,endIndex) :plantdata.plants ;
+   
     res.json(result);
 })
 
