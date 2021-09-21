639446668650186ce7334ab08d12aa03959fe29e
Paichato <tsenane14@gmail.com>
2021-09-22 00:54:42 +0200

    added plants env

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express=require('express');
  const plantdata=require('./model/plantdata.json');
  const paginate=require('./middleware/paginate')
-
+const envs=require('./model/server.json')
 const app=express();
 
 // app.use(paginate);
@@ -11,6 +11,10 @@ let port=process.env.PORT || 3000;
 app.get("/plants",paginate(plantdata.plants) ,(req,res)=>{
 
     res.json(res.paginatedResult);
+});
+
+app.get('plants_environments',(req,res)=>{
+    res.json(envs.plants_environments);
 })
 
 // app.post("/plant",paginate)
